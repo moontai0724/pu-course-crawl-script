5155d263971e17ac16c14941be3f584b3fb58cba
moontai0724 <moontai0724@gmail.com>
2023-05-24 15:10:28 +0800

    chore: init project

---
+++ HEADER.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         An Unnamed Script
+// @namespace    https://github.com/moontai0724
+// @version      1.0.0
+// @description  TBD
+// @author       moontai0724
+// @match        https://*/*
+// @grant        none
+// @supportURL   https://github.com/moontai0724/
+// @license      MIT
+// ==/UserScript==
