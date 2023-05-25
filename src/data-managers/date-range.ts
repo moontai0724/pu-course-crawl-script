6e8a6b5a139ba843f731e540c3778b5f089710ea
moontai0724 <moontai0724@gmail.com>
2023-05-25 16:58:05 +0800

    feat(data-manager): date range

---
+++ src/data-managers/date-range.ts
@@ -0,0 +1,19 @@
+import { DateRange } from "_types/schema";
+
+const dateRanges: Omit<DateRange, "uuid">[] = [
+  {
+    id: 1,
+    name: "112-1",
+    description: null,
+    startDate: new Date("2023-09-11"),
+    endDate: new Date("2024-01-15"),
+  },
+];
+
+export function getAll() {
+  return dateRanges;
+}
+
+export function getOne(id: number) {
+  return getAll().find(dateRange => dateRange.id === id);
+}
