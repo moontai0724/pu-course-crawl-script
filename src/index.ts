0f6d0eb5b4e53c617b787265ac6c033da13f2266
moontai0724 <moontai0724@gmail.com>
2023-05-25 23:18:24 +0800

    feat: log and download

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,50 @@ import {
   Type,
 } from "./data-managers";
 
-DateRange.getAll();
-Organization.getAll();
-Person.getAll();
-Place.getAll();
-Time.getAll();
-Type.getAll();
+const rDateRange = DateRange.getAll();
+const rOrganization = Organization.getAll();
+const rPerson = Person.getAll();
+const rPlace = Place.getAll();
+const rTime = Time.getAll();
+const rType = Type.getAll();
+const rCourse = Course.getAll();
 
-Course.getAll();
+const oDateRange = DateRange.getInputs();
+const oOrganization = Organization.getInputs();
+const oPerson = Person.getInputs();
+const oPlace = Place.getInputs();
+const oTime = Time.getInputs();
+const oType = Type.getInputs();
+const oCourse = Course.getInputs();
 
-console.log("courses data", Course.getInputs());
-console.log("dateRanges data", DateRange.getInputs());
-console.log("organizations data", Organization.getInputs());
-console.log("persons data", Person.getInputs());
-console.log("places data", Place.getInputs());
-console.log("timeRanges data", Time.getInputs());
-console.log("types data", Type.getInputs());
+console.log("courses data", rDateRange, oDateRange);
+console.log("dateRanges data", rOrganization, oOrganization);
+console.log("organizations data", rPerson, oPerson);
+console.log("persons data", rPlace, oPlace);
+console.log("places data", rTime, oTime);
+console.log("timeRanges data", rType, oType);
+console.log("types data", rCourse, oCourse);
+
+download("courses.json", JSON.stringify(oCourse));
+download("dateRanges.json", JSON.stringify(oDateRange));
+download("organizations.json", JSON.stringify(oOrganization));
+download("persons.json", JSON.stringify(oPerson));
+download("places.json", JSON.stringify(oPlace));
+download("timeRanges.json", JSON.stringify(oTime));
+download("types.json", JSON.stringify(oType));
+
+function download(filename: string, text: string) {
+  const element = document.createElement("a");
+  element.setAttribute(
+    "href",
+    "data:application/json;charset=utf-8," + encodeURIComponent(text),
+  );
+  element.setAttribute("download", filename);
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
