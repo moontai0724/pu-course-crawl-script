eaea7e407ca9d7bbd979ac1fe20f10a77134ae3c
moontai0724 <moontai0724@gmail.com>
2023-05-25 17:30:52 +0800

    fix: bypass table header when parsing

--- src/data-managers/course.data-manager.ts
+++ src/data-managers/course.data-manager.ts
@@ -20,67 +20,69 @@ export function getAll(): typeof courses {
   if (courses.length) return courses;
 
   const elements = document.querySelectorAll("table tr");
-  Array.from(elements).forEach((element, id) => {
-    const [
-      codeElement,
-      organizationElement,
-      typeElement,
-      titleElement,
-      semesterElement,
-      creditElement,
-      personElement,
-      timeAndPlaceElement,
-      noteElement,
-    ] = Array.from(element.querySelectorAll("td"));
+  Array.from(elements)
+    .slice(1)
+    .forEach((element, id) => {
+      const [
+        codeElement,
+        organizationElement,
+        typeElement,
+        titleElement,
+        semesterElement,
+        creditElement,
+        personElement,
+        timeAndPlaceElement,
+        noteElement,
+      ] = Array.from(element.querySelectorAll("td"));
 
-    const code = codeElement.textContent?.trim() ?? "";
-    const [name, englishName] =
-      titleElement.textContent?.trim().split("\n") ?? "";
-    const description =
-      englishName + "\n" + noteElement.textContent?.trim() ?? "";
-    const linkElement = titleElement.querySelector("a");
-    const link = linkElement?.href.replace("../", "https://alcat.pu.edu.tw/");
-    const credit = parseInt(creditElement.textContent?.trim() ?? "0", 10);
+      const code = codeElement.textContent?.trim() ?? "";
+      const [name, englishName] =
+        titleElement.textContent?.trim().split("\n") ?? "";
+      const description =
+        englishName + "\n" + noteElement.textContent?.trim() ?? "";
+      const linkElement = titleElement.querySelector("a");
+      const link = linkElement?.href.replace("../", "https://alcat.pu.edu.tw/");
+      const credit = parseInt(creditElement.textContent?.trim() ?? "0", 10);
 
-    const organization = Organization.getByElement(organizationElement);
-    const type = Type.getByElement(typeElement);
-    const timeRanges = Time.getByElement(timeAndPlaceElement);
-    const places = Place.getByElement(timeAndPlaceElement);
-    const host = Person.getByElement(personElement.querySelector("a"));
+      const organization = Organization.getByElement(organizationElement);
+      const type = Type.getByElement(typeElement);
+      const timeRanges = Time.getByElement(timeAndPlaceElement);
+      const places = Place.getByElement(timeAndPlaceElement);
+      const host = Person.getByElement(personElement.querySelector("a"));
 
-    const course: RawCourse = {
-      code,
-      name,
-      description,
-      link: link ?? null,
-      credit,
-      organizationId: organization?.id ?? null,
-      dateRangeId: 1,
-      relations: {
-        timeRangeIds: [],
-        placeIds: [],
-        tagIds: [],
-        personIds: [],
-      },
-    };
+      const course: RawCourse = {
+        code,
+        name,
+        description,
+        link: link ?? null,
+        credit,
+        organizationId: organization?.id ?? null,
+        dateRangeId: 1,
+        relations: {
+          timeRangeIds: [],
+          placeIds: [],
+          tagIds: [],
+          personIds: [],
+        },
+      };
 
-    if (type) course.relations.tagIds.push(type.id);
-    if (host) course.relations.personIds.push(host.id);
-    if (timeRanges.length) {
-      const timeRangeIds = timeRanges
-        .map(time => time?.id)
-        .filter(Boolean) as number[];
-      course.relations.timeRangeIds.push(...timeRangeIds);
-    }
-    if (places.length) {
-      const placeIds = places
-        .map(place => place?.id)
-        .filter(Boolean) as number[];
-      course.relations.placeIds.push(...placeIds);
-    }
+      if (type) course.relations.tagIds.push(type.id);
+      if (host) course.relations.personIds.push(host.id);
+      if (timeRanges.length) {
+        const timeRangeIds = timeRanges
+          .map(time => time?.id)
+          .filter(Boolean) as number[];
+        course.relations.timeRangeIds.push(...timeRangeIds);
+      }
+      if (places.length) {
+        const placeIds = places
+          .map(place => place?.id)
+          .filter(Boolean) as number[];
+        course.relations.placeIds.push(...placeIds);
+      }
 
-    courses.push(course);
-  });
+      courses.push(course);
+    });
 
   return courses;
 }
